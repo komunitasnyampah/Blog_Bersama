6b194a6274223a279908fa957016e510990044cc
komunitasnyampah <nyampahbersama@newsletterss.com>
2025-07-03 23:00:09 +0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -7,7 +7,12 @@ export default function Navbar() {
       background: 'var(--primary)', padding: '1rem 2rem', color: '#fff'
     }}>
       <div style={{ fontWeight: 'bold', fontSize: 22 }}>
-        <img src="/logo.png" alt="Logo" style={{ height: 40, verticalAlign: 'middle' }} /> Komunitas
+        <img src="https://ipfs.io/ipfs/bafybeiazgojdslj6gabeljszahwddnqpaz7ugvfn4egwjucoh6vgdkadoy"
+    alt="Gambar IPFS"
+     width={400}
+     height={300}
+      />" 
+       alt="Logo" style={{ height: 40, verticalAlign: 'middle' }} /> Komunitas
       </div>
       <div>
         <Link href="/">Home</Link> | <Link href="/portfolio">Portofolio</Link>
